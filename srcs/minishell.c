fd2e40a374ce17b50ebfce264937d6eee79fe3a0
Ersin Ozben <eozben@1-A-10.42heilbronn.de>
2021-11-17 21:45:59 +0100

    tokenizer

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: eozben <eozben@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 20:31:46 by eozben            #+#    #+#             */
-/*   Updated: 2021/11/16 21:36:05 by eozben           ###   ########.fr       */
+/*   Updated: 2021/11/17 21:45:21 by eozben           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,43 @@ void	print_list(t_tok *head)
 	}
 }
 
-void	read_toks(t_tok *head)
+void	skip_whitespace(char *input)
 {
-	t_
+	while (*input == ' ' || *input == '\n' || *input == '\t')
+		input++;
+}
+
+t_token	check_type(char *c)
+{
+	if (c[0] == SQUOTE || c[0] == DQUOTE || c[0] == LPAREN
+		|| c[0] == RPAREN)
+		return (c[0]);
+	else if (c[0] == PIPE || c[0] == AMPERSAND || c[0] == GREAT || c[0] == LESS)
+	{
+		if (c[0] == c[1])
+			return (c[0] * 4);
+		return(c[0]);
+	}
+	else
+		return (WORD);
+}
+
+void	read_toks(t_tok *head, char *input)
+{
+	t_token	tok;
+
+	while (head != NULL)
+	{
+		skip_whitespace(input);
+		head->type = check_type(input);
+		if (head->type > 127)
+			input += 2;
+		else if (head->type != 0)
+			input++;
+		else
+			
+		head = head->next;
+	}
 }
 
 int	main(void)
@@ -37,10 +71,11 @@ int	main(void)
 		read = readline("minish $ ");
 		if (!ft_strcmp(read, "\0"))
 			add_history(read);
-		read_toks(head);
+		read_toks(head, read);
 		free(read);
 		if (ft_strcmp(read, "exit"))
 			break ;
 	}
+	check_meta(' ');
 	return (0);
 }
