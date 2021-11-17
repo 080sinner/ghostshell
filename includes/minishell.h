9ef53cd8f733f3b26030bf03c709db4be2d34877
Ersin Ozben <eozben@1-A-10.42heilbronn.de>
2021-11-17 21:56:22 +0100

    florian du noob

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: eozben <eozben@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 20:32:45 by eozben            #+#    #+#             */
-/*   Updated: 2021/11/16 21:35:14 by eozben           ###   ########.fr       */
+/*   Updated: 2021/11/17 21:45:43 by eozben           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,49 +21,56 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-# define GREAT 401
-# define LESS 402
-# define GREATGREAT 403
-# define LESSLESS 404
-# define PIPE 405
+
+
+// typedef struct s_lexer
+// {
+// 	t_tok	*tok_list;
+// 	int		ntoks;
+// }				t_lexer;
+
+// typedef struct s_tree_node
+// {
+// 	char				data;
+// 	struct s_tree_node	*left;
+// 	struct s_tree_node	*right;
+// }				t_tree;
+
+// typedef struct s_cmd_table
+// {
+// 	char				**cmd;
+// 	int					input;
+// 	int					output;
+// 	int					cmd_amount;
+// 	struct s_cmd_table	*next;
+// 	struct s_cmd_table	*prev;
+// }				t_cmd_table;
+
+typedef enum e_token
+{
+	WORD = 0,
+	SPACE = ' ',
+	SQUOTE = '\'',
+	DQUOTE = '\"',
+	PIPE = '|',
+	LPAREN = '(',
+	RPAREN = ')',
+	GREAT = '>',
+	LESS = '<',
+	AND = 152,
+	LESSLESS = 240,
+	GREATGREAT = 248,
+	OR = 496,
+	AMPERSAND = '&',
+}			t_token;
 
 typedef struct s_tok
 {
 	char			*data;
-	int				type;
+	t_token			type;
 	struct s_tok	*next;
 	struct s_tok	*previous;
 }				t_tok;
-
-typedef struct s_lexer
-{
-	t_tok	*tok_list;
-	int		ntoks;
-}				t_lexer;
-
-typedef struct s_tree_node
-{
-	char				data;
-	struct s_tree_node	*left;
-	struct s_tree_node	*right;
-}				t_tree;
-
-typedef struct s_cmd_table
-{
-	char				**cmd;
-	int					input;
-	int					output;
-	int					cmd_amount;
-	struct s_cmd_table	*next;
-	struct s_cmd_table	*prev;
-}				t_cmd_table;
-
-typedef enum e_metacharacters
-{
-	space = ' ',
-	squote = '\'',
-	dquote = '\"'
-}			t_meta;
 
 t_tok	*ft_last_element(t_tok *head);
 void	ft_dll_attach_tok(t_tok **head, t_tok *attachment);
