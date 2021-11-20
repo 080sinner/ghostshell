a9864cbcd20b279c689a46381721a2eaa9103bd4
080sinner <ersinoezben@icloud.com>
2021-11-20 05:12:12 +0100

    no leaks

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 20:47:32 by fbindere          #+#    #+#             */
-/*   Updated: 2021/11/20 05:03:06 by mac              ###   ########.fr       */
+/*   Updated: 2021/11/20 05:10:56 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,31 +22,59 @@ static void	print_list(t_tok *head)
 	}
 }
 
-int	main(void)
+void get_input(t_tok **head, t_tok **save)
 {
-	t_tok	*head;
-	t_tok	*save;
 	char	*read;
 
-	head = NULL;
 	while (1)
 	{
 		read = readline("minish $ ");
 		if (!ft_strcmp(read, "\0"))
 			add_history(read);
-		save = read_toks(&head, read);
 		if (ft_strcmp(read, "exit"))
 		{
 			free(read);
 			break ;
 		}
+		*save = read_toks(head, read);
 		free(read);
 	}
+}
+
+int	main(void)
+{
+	t_tok	*head;
+	t_tok	*save;
+
+	head = NULL;
+	save = NULL;
+	get_input(&head, &save);
 	print_list(head);
 	free_list(save);
 	free_dll(&head);
-	head = NULL;
 	save = NULL;
+	head = NULL;
 	system("leaks minishell");
 	return (0);
 }
+
+// typedef struct s_treenode{
+// 	t_token tok;
+// 	struct s_treenode *left;
+// 	struct s_treenode *right;
+// }				t_treenode;
+
+
+// t_treenode	*create_tree_node(t_token token)
+// {
+// 	t_treenode	*new;
+
+// 	new = malloc(sizeof(t_treenode));
+// 	if (new != NULL)
+// 	{
+// 		new->left = NULL;
+// 		new->right = NULL;
+// 		new->tok = token;
+// 	}
+// 	return (new);
+// }
