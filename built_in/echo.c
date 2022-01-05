92c0c885572b7d376eafba2aea3b647ddcc6bbd7
Florian Bindereif <fbindere@1-A-6.42heilbronn.de>
2022-01-06 00:55:27 +0100

    also echo

--- built_in/echo.c
+++ built_in/echo.c
@@ -6,30 +6,51 @@
 /*   By: fbindere <fbindere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 21:52:34 by fbindere          #+#    #+#             */
-/*   Updated: 2022/01/05 21:52:53 by fbindere         ###   ########.fr       */
+/*   Updated: 2022/01/06 00:53:27 by fbindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	echo(char **args)
+int check_newline_flag(int *i, char **args)
 {
-	int	i;
+	int j;
+	int newline_flag;
+	newline_flag = 1;
+	while (args[*i] && args[*i][0] == '-')
+	{
+		j = 1;
+		while (args[*i][j])
+		{
+			if (args[*i][j] == 'n')
+				j++;
+			else
+				break ;
+		}
+		if (args[*i][j] == '\0')
+			newline_flag = 0;
+		else
+			break ;
+		*i += 1;
+	}
+	return (newline_flag);
+}
 
+int echo(char **args)
+{
+	int i;
+	int newline_flag;
+	newline_flag = 1;
 	i = 1;
 	if (!args[i])
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	while (!ft_strncmp(args[i], "-n", 2))
-		i++;
+	newline_flag = check_newline_flag(&i, args);
 	while (args[i])
-	{
-		printf("%s ", args[i]);
-		i++;
-	}
-	if (ft_strncmp(args[1], "-n", 2))
+		printf("%s ", args[i++]);
+	if (newline_flag)
 		printf("\n");
 	return (0);
 }
--- srcs/here_doc.c
+++ srcs/here_doc.c
@@ -6,84 +6,11 @@
 /*   By: fbindere <fbindere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 20:57:30 by fbindere          #+#    #+#             */
-/*   Updated: 2022/01/05 20:26:42 by fbindere         ###   ########.fr       */
+/*   Updated: 2022/01/06 00:48:10 by fbindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-char	*combine_strings(char *front, char *variable, char *end)
-{
-	char	*temp[2];
-	char	*new;
-
-	if (ft_getenv(variable, g_utils.environment) != NULL)
-		temp[1] = ft_strdup(ft_getenv(variable, g_utils.environment));
-	else
-		temp[1] = ft_strdup("");
-	ft_free((void *)&variable, ft_strlen(variable));
-	temp[0] = ft_strjoin(front, temp[1]);
-	ft_free((void *)&temp[1], ft_strlen(temp[1]));
-	ft_free((void *)&front, ft_strlen(front));
-	new = ft_strjoin(temp[0], end);
-	free(temp[0]);
-	free(end);
-	return (new);
-}
-
-int	contains_variable(char *string)
-{
-	int	i;
-
-	i = 0;
-	while (string[i] != '\0')
-	{
-		if (string[i] == '$' && (ft_isalnum(string[i + 1]) 
-				|| string[i + 1] == '_'))
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int		expand_here_doc(t_tok *here_doc)
-{
-	char	*front;
-	char	*variable;
-	char	*end;
-	t_tok 	*current;
-	int		i;
-	int		varlen;
-
-	current = here_doc;
-	while (current)
-	{
-		while (contains_variable(current->data))
-		{
-			i = 0;
-			while (current->data[i] != '$')
-				i++;
-			varlen = i;
-			while (current->data[i] == '$')
-				i++;
-			i = varlen;
-			front = ft_substr(current->data, 0, i);
-			i++;
-			varlen = 0;
-			while (ft_isalnum(current->data[i]) || current->data[i] == '_')
-			{
-				i++;
-				varlen++;
-			}
-			variable = ft_substr(ft_strchr(current->data, '$'), 1, varlen);
-			end = ft_substr(current->data, i, ft_strlen(current->data) - 1);
-			free(current->data);
-			current->data = combine_strings(front, variable, end);
-		}
-		current = current->next;
-	}
-	return (0);
-}
 
 char	*convert_variable_delimiter(char *data)
 {
