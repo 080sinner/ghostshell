95be5f9871f5a592fb17348d76f99d057d2c5405
Florian Bindereif <fbindere@1-A-6.42heilbronn.de>
2022-01-10 20:59:27 +0100

    tester included

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fbindere <fbindere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 20:47:32 by fbindere          #+#    #+#             */
-/*   Updated: 2022/01/09 18:53:12 by fbindere         ###   ########.fr       */
+/*   Updated: 2022/01/10 20:57:12 by fbindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,38 @@ static int	check_empty_input(char *input)
 		return (0);
 }
 
+int	get_next_line(char **line, int fd)
+{
+	char	buffer;
+	int		flag;
+
+	if(line == NULL)
+		return(-1);
+	*line = malloc(1);
+	if(*line == NULL)
+		return(-1);
+	*line[0] = '\0';
+	while ((flag = read(fd, &buffer, 1) > 0))
+	{
+		if(buffer == '\n')
+			break;
+		*line = ft_append(*line, buffer);
+	}
+	return(flag);		
+}
+
 static void	ghosthell(t_node **head)
 {
 	char	*read;
 
-	print_ghostshell();
+	//print_ghostshell();
 	while (1)
 	{
 		signal_handler();
+		
 		read = readline("\e[1m	\033[1;34m༼ つ ❍_❍ ༽つ\033[0m\e[0m	");
+		// if (get_next_line(&read, STDIN_FILENO) == 0)
+		// 	read = NULL;
 		clear_signals();
 		signal(SIGINT, SIG_IGN);
 		signal(SIGQUIT, SIG_IGN);
