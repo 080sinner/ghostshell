1bb97fa9cf522c7cb4ee3c2535eb64b78f7abc92
Florian Bindereif <fbindere@1-A-4.42heilbronn.de>
2022-01-15 17:04:02 +0100

    mandatory fine

--- executor/get_cmd_path.c
+++ executor/get_cmd_path.c
@@ -6,7 +6,7 @@
 /*   By: fbindere <fbindere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 16:28:42 by eozben            #+#    #+#             */
-/*   Updated: 2022/01/14 21:10:18 by fbindere         ###   ########.fr       */
+/*   Updated: 2022/01/15 16:57:18 by fbindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ static int	cmd_not_found(char **paths, char *command)
 		ft_putendl_fd(" is a directory", 2);
 		g_utils.exit_status = 126;
 	}
-	else if (!paths)
+	else if (access(command, F_OK))
 	{
 		ft_putendl_fd("No such file or directory", 2);
 		g_utils.exit_status = 127;
@@ -85,6 +85,17 @@ static int	cmd_not_found(char **paths, char *command)
 	return (ERROR);
 }
 
+static int	is_direct_path(char *data, char **paths)
+{
+	if (!paths)
+		return (1);
+	if (ft_strchr(data, '/'))
+		return (1);
+	if (ft_strcmp(data, ".") || ft_strcmp(data, ".."))
+		return (1);
+	return (0);
+}
+
 int	get_cmd_path(t_node *command)
 {
 	char		**paths;
@@ -97,7 +108,8 @@ int	get_cmd_path(t_node *command)
 	data = command->args->data;
 	if (check_builtin(command->args))
 		return (1);
-	if (ft_strchr(data, '/') || ft_strcmp(data, ".") || ft_strcmp(data, ".."))
+	paths = ft_split(ft_getenv("PATH", g_utils.environment), ':');
+	if (is_direct_path(data, paths))
 	{
 		if (!access(data, F_OK) && !stat(data, &s) && !(s.st_mode & S_IFDIR)) 
 		{
@@ -106,9 +118,8 @@ int	get_cmd_path(t_node *command)
 		}
 		return(cmd_not_found(paths, command->args->data));
 	}
-	paths = ft_split(ft_getenv("PATH", g_utils.environment), ':');
-	if (!paths  || append_slash(paths, "/") == ERROR)
-		return (cmd_not_found(paths, command->args->data));
+	if (append_slash(paths, "/") == ERROR)
+		return (ERROR);
 	if (cmdpath(command, paths) == ERROR)
 		return (cmd_not_found(paths, command->args->data));
 	return (1);
